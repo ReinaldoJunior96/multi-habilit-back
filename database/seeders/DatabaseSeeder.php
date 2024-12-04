0a348bb1d8da08d0406c0f813cc23ca4d054f1fd
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2024-12-04 15:37:12 -0300

    ajustando agendamento

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,14 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UsuariosTableSeeder::class,
-            MedicoSeeder::class,
-            EnderecosTableSeeder::class,
-            //AtendentesTableSeeder::class,
-            PacientesTableSeeder::class, // Pacientes precisam existir antes dos convênios
-            ConveniosTableSeeder::class, // Convenios precisam ser criados antes dos agendamentos
-            //AgendamentosTableSeeder::class, // Depende de convenios
-            EnderecoSeeder::class,
+            UsuariosTableSeeder::class,          // Cria os usuários
+            AtendentesTableSeeder::class,        // Depende dos usuários
+            MedicoSeeder::class,                 // Médicos independentes
+            EnderecosTableSeeder::class,         // Endereços para usuários/médicos
+            PacientesTableSeeder::class,         // Pacientes antes dos convênios
+            ConveniosTableSeeder::class,         // Convênios antes dos agendamentos
+            AgendamentosTableSeeder::class,      // Agendamentos dependem de convênios
         ]);
     }
 }
