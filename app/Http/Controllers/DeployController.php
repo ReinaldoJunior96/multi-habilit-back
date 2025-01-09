544c6b307ac0783c38c0b5051efc4d6b1830ca51
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2025-01-09 18:11:29 -0300

    adicionando deploy automatico

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -13,7 +13,7 @@ class DeployController extends Controller
 
         // Executa o comando de deploy
         $output = [];
-        exec("cd {$repoPath} && git pull origin develop", $output);
+        exec("cd {$repoPath} && git pull origin main", $output);
 
         // Retorna o resultado do deploy
         return response()->json([
