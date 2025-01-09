a885c4f3273eec82e0a06209efe8090575400da2
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2025-01-09 17:58:22 -0300

    adicionando deploy automatico

--- app/Http/Controllers/HorarioController.php
+++ app/Http/Controllers/HorarioController.php
@@ -11,7 +11,7 @@ class HorarioController extends Controller
     // Listar todos os horários
     public function index()
     {
-        dd("testando git pull novamente");
+        dd("testando git pull novamente sera?");
         $horarios = Horario::with('medico.usuario')->get();
         return response()->json($horarios, 200);
     }
