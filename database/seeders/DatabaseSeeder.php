e7a142e5db0cce6de901d8d930d9686064401bf9
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2025-01-12 11:01:50 -0300

    ajustando horarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsuariosTableSeeder::class,          // Cria os usuários
             MedicoSeeder::class,                 // Médicos independentes
-            //HorarioSeeder::class,
-            //PacientesTableSeeder::class,         // Pacientes antes dos convênios
-            //ConveniosTableSeeder::class,
-            //ProcedimentoSeeder::class,
-            //ConvenioProcedimentoSeeder::class,         // Convênios antes dos agendamentos
+            HorarioSeeder::class,
+            PacientesTableSeeder::class,         // Pacientes antes dos convênios
+            ConveniosTableSeeder::class,
+            ProcedimentoSeeder::class,
+            ConvenioProcedimentoSeeder::class,         // Convênios antes dos agendamentos
             //AgendamentosTableSeeder::class,      // Agendamentos dependem de convênios
         ]);
     }
