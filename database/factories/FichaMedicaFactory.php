7723609bfebab95ea37f8222876075d33f6885cd
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2025-05-20 15:18:12 -0300

    ajuste

--- database/factories/FichaMedicaFactory.php
+++ database/factories/FichaMedicaFactory.php
@@ -13,15 +13,91 @@ class FichaMedicaFactory extends Factory
     public function definition()
     {
         return [
-            'id_paciente' => Paciente::factory(), // Cria um paciente se não houver
+            'id_paciente' => Paciente::factory(),
+
             'ficha' => [
-                'altura' => $this->faker->randomFloat(2, 1.5, 2),
-                'peso' => $this->faker->randomFloat(1, 40, 120),
-                'historico' => $this->faker->paragraph,
-                'alergias' => $this->faker->words(3),
-                'medicamentos_uso_continuo' => $this->faker->words(2),
-                'pressao_arterial' => $this->faker->randomElement(['120/80', '130/85', '140/90']),
-            ],
+                'registro_ans' => $this->faker->numerify('########'),
+                'numero_guia_principal' => $this->faker->uuid,
+                'data_autorizacao' => $this->faker->date(),
+                'senha' => $this->faker->bothify('SENHA####'),
+                'data_validade_senha' => $this->faker->date('+1 month'),
+                'numeroGuiaAtribuidoPelaOperadora' => $this->faker->uuid,
+                'numeroCarteira' => $this->faker->numerify('##########'),
+                'validadeCarteira' => $this->faker->date('+1 year'),
+                'nomeSocial' => $this->faker->name,
+                'nome' => $this->faker->name,
+                'atendimentoRn' => $this->faker->boolean,
+
+                'codigoOperadora' => $this->faker->numerify('OPER###'),
+                'nomeContratado' => $this->faker->company,
+                'nomeProfissionalSolicitante' => $this->faker->name,
+                'conselhoProfissional' => 'CRM',
+                'numeroConselho' => $this->faker->numerify('######-SP'),
+                'uf' => 'SP',
+                'codigoCbo' => $this->faker->numerify('######'),
+                'assinaturaProfissionalSolicitante' => 'assinatura_base64',
+
+                'carater_atendimento' => $this->faker->randomElement(['Eletivo', 'Urgência']),
+                'data_solicitacao' => $this->faker->date(),
+                'indicacao_clinica' => $this->faker->sentence,
+                'indicador_cobertura_especial' => $this->faker->boolean,
+
+                'procedimento' => [
+                    [
+                        'tabela' => '22',
+                        'codigo' => '12345678',
+                        'descricao' => 'Consulta médica',
+                        'qtde_solicitada' => 1,
+                        'qtde_autorizada' => 1,
+                    ]
+                ],
+
+                'codigo_operadora_contrato_executante' => $this->faker->numerify('EXE###'),
+                'nome_contratado_dados_contrato_executante' => $this->faker->company,
+                'codigoCnes' => $this->faker->numerify('#######'),
+
+                'tipoAtendimento' => 'Ambulatorial',
+                'indicacaoAcidente' => 'Não',
+                'tipoConsulta' => 'Primeira vez',
+                'motivoEncerramentoAtendimento' => 'Tratamento concluído',
+                'regimeAtendimento' => 'Hospital dia',
+                'saudeOcupacional' => $this->faker->boolean,
+
+                'data' => $this->faker->date(),
+                'horaInicial' => '08:00',
+                'horaFinal' => '08:30',
+                'tabela' => '22',
+                'codigoProcedimento' => '998877',
+                'descricao' => 'Procedimento Exemplo',
+                'quantidade' => 1,
+                'via' => 'Oral',
+                'tec' => 'Simples',
+                'fatorRedAcresc' => 1.0,
+                'valorUnitario' => 100.0,
+                'valorTotal' => 100.0,
+
+                'seqRef' => '001',
+                'grauPart' => '1',
+                'codigoOperadoraIPE' => 'IPE123',
+                'nomeProfissional' => $this->faker->name,
+                'conselhoProfissionalIdentificacao' => 'CRM',
+                'numeroConselhoIdentificacao' => $this->faker->numerify('######-SP'),
+                'ufIdentificacao' => 'SP',
+                'codigoCboIdentificacao' => $this->faker->numerify('######'),
+                'dataRealizacaoProcedimentoEmSerie' => $this->faker->date(),
+                'assinaturaBeneficiarioResponsavel' => 'assinatura_base64',
+                'observacaoJustificacao' => $this->faker->sentence,
+                'totalProcedimento' => 100.0,
+                'totalTaxasAlugueis' => 0.0,
+                'totalMateriais' => 0.0,
+                'totalOpme' => 0.0,
+                'totalMedicamentos' => 0.0,
+                'totalGasesMedicinais' => 0.0,
+                'totalGeral' => 100.0,
+                'assinaturaResponsavelAutorizacao' => 'assinatura_base64',
+                'assinaturaBeneficiarioResponsavelFinal' => 'assinatura_base64',
+                'assinaturaContrato' => 'assinatura_base64',
+            ]
         ];
     }
 }
--- database/seeders/PacientesTableSeeder.php
+++ database/seeders/PacientesTableSeeder.php
@@ -4,11 +4,19 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Paciente;
+use App\Models\FichaMedica;
 
 class PacientesTableSeeder extends Seeder
 {
     public function run()
     {
-        Paciente::factory()->count(50)->create();
+        Paciente::factory()
+            ->count(10)
+            ->create()
+            ->each(function ($paciente) {
+                FichaMedica::factory()->create([
+                    'id_paciente' => $paciente->id
+                ]);
+            });
     }
 }
